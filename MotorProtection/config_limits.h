e1fa717f0590dacac4f3394f7573ec4a786078d3
ISHRAQ6251 <149072037+ISHRAQ6251@users.noreply.github.com>
2026-09-22 05:13:50 +0600

    Update config_limits.h

--- MotorProtection/config_limits.h
+++ MotorProtection/config_limits.h
@@ -12,6 +12,7 @@ static const int AUTH_USER_LEN = 32;
 static const int AUTH_PASS_LEN = 32;
 
 static const uint8_t CH_UNUSED = 0xFF;
+static const uint8_t VCH_SAME  = 0xFF;
 
 static const int RMS_SAMPLES     = 32;
 static const int CAL_SAMPLES     = 64;
@@ -20,9 +21,11 @@ static const float VADC_MIN      = 0.05f;
 static const float VADC_MAX      = 3.05f;
 
 static const int V_CAL_SAMPLES     = 16;
+static const int V_AVG_SAMPLES     = 4;
 static const float VBUS_CAP        = 55.0f;
 static const float VADC_ABS_MAX    = 4.0f;
-static const uint32_t UV_GRACE_MS  = 250;
+static const uint32_t UV_GRACE_MS  = 750;
+static const uint32_t VOLT_LOG_MS  = 2000;
 
 static const float NO_CURRENT_FRAC  = 0.05f;
 static const uint32_t NO_CURRENT_MS = 2000u;
@@ -40,7 +43,7 @@ static const uint32_t JAM_RELEASE_MIN_RUN_MS = 500;
 static const int LOG_RING = 32;
 
 static const uint32_t NVS_MAGIC   = 0x4D505331u;
-static const uint16_t NVS_SCHEMA  = 3;
+static const uint16_t NVS_SCHEMA  = 4;
 
 static const char *NVS_NS        = "mps";
 static const char *NVS_KEY_BLOB  = "motors";
